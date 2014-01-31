87ce465a1fe7859b4a18784954916fd77b4b9a45
suizokukan <suizokukan@orange.fr>
2014-01-31 16:25:09 +0100

    added a splashscreen

---
+++ qt/splashscreen.py
@@ -0,0 +1,73 @@
+#!./python_link
+# -*- coding: utf-8 -*-
+################################################################################
+#    Dipylon Copyright (C) 2008 Xavier Faure
+#    Contact: faure dot epistulam dot mihi dot scripsisti at orange dot fr
+#
+#    This file is part of Dipylon.
+#    Dipylon is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Dipylon is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Dipylon.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+        ❏Dipylon❏ splashscreen.py
+"""
+
+from Qt5 import QtGui
+
+from system import numversion as numversion
+import system.communia
+VERSION = numversion.VersionOfTheProgram().numversion
+
+from qt import constants
+
+################################################################################
+class SplashScreen(QtGui.QtWidgets.QSplashScreen):
+    """
+        class SplashScreen
+    """
+
+    message = "<span style=\"color:#223399\">" \
+              "<b>Dipylon</b>'s version={0}<br/>" \
+
+    #///////////////////////////////////////////////////////////////////////////
+    def __init__(self):
+        """
+                SplashScreen.__init__
+        """
+        splash_image = QtGui.QPixmap( os.path.join('images', 'splashscreen.png'))
+
+        QtGui.QSplashScreen.__init__(self, splash_image, QtCore.Qt.WindowStaysOnTopHint)
+
+        self.setWindowTitle(constants.GENERIC_WINDOW_TITLE)
+
+        msg = SplashScreen.message.format(VERSION)
+
+        self.showMessage(msg,
+		                 QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        
+        self.mousePressEvent = self.event__mousepress
+        self.keyPressEvent = self.event__keypressevent
+
+    #///////////////////////////////////////////////////////////////////////////
+    def event__mousepress(self, event):
+        """
+                SplashScreen.event__mousepress
+        """
+        self.close()
+
+    #///////////////////////////////////////////////////////////////////////////
+    def event__keypressevent(self, event):
+        """
+                SplashScreen.event__keypressevent
+        """
+        self.close()
