d217e8d4db38a350528bb5a56a0e095d17e1a4f5
suizokukan <suizokukan@orange.fr>
2014-05-20 16:42:10 +0200

    v. 0.1.2 : editors' heights in the main splitter zone

--- dipylon/main.cpp
+++ dipylon/main.cpp
@@ -16,6 +16,17 @@ int main(int argv, char **args)
     MainSplitter *main_splitter = new MainSplitter;
     main_splitter->addWidget(src_editor);
     main_splitter->addWidget(com_editor);
+
+    /* ne pas régler la taille dans le constructeur : d'abord ajouter les widgets puis
+       régler la taille : 
+
+       Avec (500, 100) comme valeurs, il existe un rapport de 5 à 1 en taille.
+       Deux nombres doivent être donnés (et pas un seul)
+    */
+    QList<int> mainsplitter_heights;
+    mainsplitter_heights << 500 << 100;
+    main_splitter->setSizes( mainsplitter_heights );
+
     main_splitter->show();
 
     return app.exec();
