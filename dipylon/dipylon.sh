00b954e24ae1494dff1b5bb03445cf8dcef8b264
suizokukan <suizokukan@orange.fr>
2014-06-03 17:05:46 +0200

    added DipylonUI class

--- dipylon/dipylon.sh
+++ dipylon/dipylon.sh
@@ -2,6 +2,9 @@ rm qrc_*.cpp
 rm moc_*.cpp
 rm *.o
 rm dipylon
+
 qmake-qt5
+
 make
+
 ./dipylon
--- dipylon/main.cpp
+++ dipylon/main.cpp
@@ -1,17 +1,43 @@
-// http://qt-project.org/doc/qt-5/qtwidgets-mainwindows-application-example.html
-
 #include <QApplication>
 
 #include "qt/mainwindow.h"
 
-int main(int argc, char *argv[])
-{
-    Q_INIT_RESOURCE(dipylon);
+class DipylonUI {
 
-    QApplication app(argc, argv);
-    app.setOrganizationName("QtProject");
-    app.setApplicationName("Application Example");
+private:
+  // attributes used to initialize the QApplication constructor :
+  int cmdline_argc = 0;
+  char** cmdline_argv = nullptr;
+
+  int* current_dipydoc = nullptr;
+
+public:
+      DipylonUI(int argc, char **argv);
+      ~DipylonUI(void);
+  int go(void);
+};
+DipylonUI::DipylonUI(int argc, char **argv) {
+  this->cmdline_argc = argc;
+  this->cmdline_argv = argv;
+}
+int DipylonUI::go(void) {
+    QApplication app(this->cmdline_argc, this->cmdline_argv);
+    app.setOrganizationName(" QtProject");
+    app.setOrganizationDomain("94.23.197.37");
+    app.setApplicationName("Dipylon");
+    app.setApplicationVersion("0.1.9");
     MainWindow mainWin;
     mainWin.show();
     return app.exec();
+}
+DipylonUI::~DipylonUI(void) {
+  delete current_dipydoc;
+}
+
+int main(int argc, char **argv)
+{
+    Q_INIT_RESOURCE(dipylon);
+
+    DipylonUI myapp(argc, argv);
+    return myapp.go();
 }
