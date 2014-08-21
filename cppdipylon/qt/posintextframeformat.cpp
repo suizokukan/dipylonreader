696cfe5d8276d7e124e279eae3943ac603c3a23a
suizokukan <suizokukan@orange.fr>
2014-08-21 16:46:13 +0200

    added qt/posintextframeformat.*

---
+++ cppdipylon/qt/posintextframeformat.cpp
@@ -0,0 +1,83 @@
+/*******************************************************************************
+
+    Dipylon Copyright (C) 2008 Xavier Faure
+    Contact: faure dot epistulam dot mihi dot scripsisti at orange dot fr
+
+    This file is part of Dipylon.
+    Dipylon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Dipylon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Dipylon.  If not, see <http://www.gnu.org/licenses/>.
+
+    ____________________________________________________________________________
+
+    ❏Dipylon❏ : qt/positionintextframeformat.cpp
+
+    See positionintextframeformat.h for the documentation
+
+    ____________________________________________________________________________
+
+*******************************************************************************/
+
+#include "posintextframeformat.h"
+
+/*______________________________________________________________________________
+
+        PosInTextFrameFormat::constructor from a source string.
+
+        See positionintextframeformat.h for a description of the POSITIONINTEXTFRAMEFORMAT_SRCSTRING_FORMAT
+______________________________________________________________________________*/
+PosInTextFrameFormat::PosInTextFrameFormat(const QString& source_string) {
+  this->_internal_state = this->init_from_string(source_string);
+  this->_well_initialized = (this->_internal_state == PosInTextFrameFormat::INTERNALSTATE::OK);
+}
+
+/*______________________________________________________________________________
+
+        PosInTextFrameFormat::init_from_string() : initialize "this" from a
+                                                 source string.
+
+        The function returns an "internal state". See positionintextframeformat.h for
+        the available values.
+______________________________________________________________________________*/
+int PosInTextFrameFormat::init_from_string(const QString& source_string) {
+
+    this->_repr = source_string;
+
+    QStringList list_of_keywords = source_string.split(this->SEPARATOR);
+
+    int res = PosInTextFrameFormat::INTERNALSTATE::OK;
+
+    for( auto &keyword : list_of_keywords) {
+
+      // spaces are not taken in account :
+      keyword.replace(" ", "");
+
+      if( keyword == "inflow") {
+        this->_position = QTextFrameFormat::InFlow;
+        continue;
+      }
+
+      if( keyword == "floatleft") {
+        this->_position = QTextFrameFormat::FloatLeft;
+        continue;
+      }
+
+      if( keyword == "floatright") {
+        this->_position = QTextFrameFormat::FloatRight;
+        continue;
+      }
+
+      res = PosInTextFrameFormat::INTERNALSTATE::BADSRCSTRING_UNKNOWNKEYWORD;
+    }
+
+    return res;
+}
---
+++ cppdipylon/qt/posintextframeformat.h
@@ -0,0 +1,111 @@
+/*******************************************************************************
+
+    Dipylon Copyright (C) 2008 Xavier Faure
+    Contact: faure dot epistulam dot mihi dot scripsisti at orange dot fr
+
+    This file is part of Dipylon.
+    Dipylon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Dipylon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Dipylon.  If not, see <http://www.gnu.org/licenses/>.
+
+    ____________________________________________________________________________
+
+    ❏Dipylon❏ : qt/posintextframeformat.h
+
+    PosInTextFrameFormat class, used to describe how an illustration is inserted
+    in a DipyDoc text.
+
+    ____________________________________________________________________________
+
+	POSINTEXTFRAMEFORMAT_SRCSTRING_FORMAT :
+
+		known keywords :
+
+                "inflow", "floatleft", "floatright"
+
+*******************************************************************************/
+
+#ifndef POSINTEXTFRAMEFORMAT_H
+#define POSINTEXTFRAMEFORMAT_H
+
+#include <QString>
+#include <QStringList>
+#include <QTextFrameFormat>
+
+/*______________________________________________________________________________
+
+  PosInTextFrameFormat class
+______________________________________________________________________________*/
+class PosInTextFrameFormat {
+
+private:
+
+  QTextFrameFormat::Position  _position;
+
+  QString                     _repr;
+
+  bool                        _well_initialized;
+  int                         _internal_state;
+
+  constexpr static const char* SEPARATOR = ";";
+
+public:
+                          PosInTextFrameFormat(void);
+                          PosInTextFrameFormat(const QString&);
+
+  int                       init_from_string(const QString&);
+  int                       internal_state(void) const;
+  QTextFrameFormat::Position position(void) const;
+  const QString&            repr(void) const;
+  bool                      well_initialized(void) const;
+
+  /*
+     INTERNALSTATE
+
+     constants used to define the internal_state attribute.
+
+     o OK
+     o NOT_YET_INITIALIZED : the object has not yet been initialized
+     o BADSRCSTRING_UNKNOWNKEYWORD : unknown keyword present in the source string.
+  */
+  enum INTERNALSTATE : int {
+    OK = 0,
+    NOT_YET_INITIALIZED = -1,
+    BADSRCSTRING_UNKNOWNKEYWORD = -2,
+  };
+
+};
+
+inline PosInTextFrameFormat::PosInTextFrameFormat(void) {
+  this->_well_initialized = false;
+  this->_internal_state = this->INTERNALSTATE::NOT_YET_INITIALIZED;
+  this->_repr = QString("");
+  this->_position = QTextFrameFormat::InFlow;
+}
+
+inline int PosInTextFrameFormat::internal_state(void) const {
+  return this->_internal_state;
+}
+
+inline QTextFrameFormat::Position PosInTextFrameFormat::position(void) const {
+  return this->_position;
+}
+
+inline const QString& PosInTextFrameFormat::repr(void) const {
+  return this->_repr;
+}
+
+inline bool PosInTextFrameFormat::well_initialized(void) const {
+  return this->_well_initialized;
+}
+
+#endif
