c747d5515fa6725c607f91988cb1f6bbef004485
suizokukan <suizokukan@orange.fr>
2014-08-25 22:50:35 +0200

    added builds/ directory

--- cppdipylon/dipylon.sh
+++ cppdipylon/dipylon.sh
@@ -1,7 +1,26 @@
+echo "=== compiling Linux > Linux ==="
+echo "==="
+echo "===   This file must be launched from the root directory,"
+echo "===   not from the build/ folder."
+echo "==="
+echo "===   Everything is compiled in the build/ folder."
+echo "==="
+echo "===   The binary file created by the compilation will"
+echo "===   copied into the builds/ folder"
+echo "==="
+echo ""
+
+echo "== cleaning the build/ folder"
 rm -rf build/*
 
+echo "== calling qmake"
 qmake-qt5 -makefile dipylon.pro
 
+echo "== calling make"
 make
 
-./build/dipylon
+echo "== copying the binary into the builds/ folder"
+cp ./build/dipylon ../builds/dipylon_linux_64bits
+
+echo "== launching the binary"
+./../builds/dipylon_linux_64bits
--- cppdipylon/dipylon2win.sh
+++ cppdipylon/dipylon2win.sh
@@ -1,6 +1,12 @@
 echo "=== cross-compiling Linux > Windows using MXE ==="
 echo "==="
-echo "===   everything will be compiled in the 2win/ folder"
+echo "===   This file must be launched from the root directory,"
+echo "===   not from the 2win/ folder."
+echo "==="
+echo "===   Everything is compiled in the 2win/ folder."
+echo "==="
+echo "===   The binary file created by the compilation will"
+echo "===   copied into the builds/ folder"
 echo "==="
 echo ""
 
@@ -27,13 +33,16 @@ rm 2win/quick_dipylon.sh*
 echo "== removing all files in 2win/build/"
 rm -rf 2win/build/*
 
-cd 2win
+cd 2win/
 echo "== now in 2win/"
 
 echo "== calling qmake"
 ~/mxe/usr/i686-pc-mingw32/qt5/bin/qmake -makefile dipylon.pro
 echo "== calling make"
 make
+
+echo "== copying the binary into the builds/ folder"
+cp ./build/dipylon.exe ../../builds/dipylon_windows_32bits
 
 echo "== now in ./"
 cd ..
