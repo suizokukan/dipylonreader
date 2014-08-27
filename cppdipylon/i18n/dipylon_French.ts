96eb744c9b34c2e8058d933a751d335bcde39b40
suizokukan <suizokukan@orange.fr>
2014-08-27 23:10:18 +0200

    added i18n/ directory

---
+++ cppdipylon/i18n/dipylon_French.ts
@@ -0,0 +1,362 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="85"/>
+        <source>About Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="86"/>
+        <source>The &lt;b&gt;Application&lt;/b&gt; example demonstrates how to write modern GUI applications using Qt, with a menu bar, toolbars, and a status bar.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="260"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="263"/>
+        <source>Create a new file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="268"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="271"/>
+        <source>Open an existing DipyDoc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="276"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="279"/>
+        <source>Save the document to disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="283"/>
+        <source>Save &amp;As...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="285"/>
+        <source>Save the document under a new name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="290"/>
+        <source>Save DipyDoc&apos;s main file as...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="291"/>
+        <source>Save DipyDoc&apos;s main file as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="296"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="298"/>
+        <source>Exit the application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="303"/>
+        <source>Cu&amp;t</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="306"/>
+        <source>Cut the current selection&apos;s contents to the clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="312"/>
+        <source>&amp;Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="315"/>
+        <source>Copy the current selection&apos;s contents to the clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="321"/>
+        <source>&amp;Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="324"/>
+        <source>Paste the clipboard&apos;s contents into the current selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="329"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="330"/>
+        <source>Show the application&apos;s About box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="334"/>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="335"/>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="353"/>
+        <source>play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="355"/>
+        <source>play...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="360"/>
+        <source>stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="362"/>
+        <source>stop...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="376"/>
+        <source>&amp;File</source>
+        <translation>&amp;FichIEr</translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="387"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="394"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="405"/>
+        <source>Ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="414"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="419"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="424"/>
+        <source>AudioControls</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="459"/>
+        <source>Unable to load a (valid) DipyDoc from &lt;b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="461"/>
+        <source>See more details below.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="492"/>
+        <source>DipyDoc loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="519"/>
+        <location filename="qt/mainwindow.cpp" line="637"/>
+        <location filename="qt/mainwindow.cpp" line="678"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="520"/>
+        <source>The document has been modified.
+Do you want to save your changes?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="638"/>
+        <location filename="qt/mainwindow.cpp" line="679"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="654"/>
+        <location filename="qt/mainwindow.cpp" line="695"/>
+        <source>File saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="183"/>
+        <source>No problem has been detected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="187"/>
+        <source>The DipyDoc has not been initialized and stays in an undefined state.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="192"/>
+        <source>The DipyDoc has not been correctly initialized but no further information can be given.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="197"/>
+        <source>The given path doesn&apos;t exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="201"/>
+        <source>The given path is a file : a DipyDoc is a folder with several files.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="206"/>
+        <source>This DipyDoc contains an error : the given path doesn&apos;t contain the expected main file, &apos;$FILENAME$&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="214"/>
+        <location filename="dipydoc/dipydoc.cpp" line="231"/>
+        <source>This DipyDoc contains an error : the given path doesn&apos;t contain the expected text file, &apos;$FILENAME$&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="222"/>
+        <source>This DipyDoc contains an error : the given path doesn&apos;t contain the expected audio file (&apos;$FILENAME$&apos;) whose name was given in the DipyDoc.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="239"/>
+        <source>The given path contain a DipyDoc document whose version ($VERSION$) is outdated.The minimal version being $MINVERSION$, please update your DipyDoc documents.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="249"/>
+        <source>The given path contain a DipyDoc document whose version ($VERSION$) is too recent.The maximal version being $MAXVERSION$, please update your DipyDoc reader.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="259"/>
+        <source>This DipyDoc contains an error : the given ratio aspect of the lettrine is zero or above. A correct value would be greater than 0.Value given in main.xml : $LETTRINEASPECTRATIO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="269"/>
+        <source>This DipyDoc contains an error : the given path doesn&apos;t contain the expected lettrine file image.In main.xml the name &apos;$LETTRINEFILENAME$&apos; appears but such a file can&apos;t be found in the given path.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="280"/>
+        <source>This DipyDoc contains an error : audiorecord.text2audio can&apos;t be initialized.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="286"/>
+        <source>This DipyDoc contains an error : audiorecord.audio2text can&apos;t be initialized.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="292"/>
+        <source>This DipyDoc contains an error : the translations&apos; data are ill-formed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="298"/>
+        <source>This DipyDoc contains an error : the document&apos;s type isn&apos;t known by this version of the software.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dipydoc/dipydoc.cpp" line="304"/>
+        <source>anomaly : unknown internal state.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="44"/>
+        <source>Old English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="45"/>
+        <source>Tibetan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="46"/>
+        <source>German</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="47"/>
+        <source>French</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="48"/>
+        <source>Ancient Greek</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="49"/>
+        <source>Biblical Hebrew</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="50"/>
+        <source>Japan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="51"/>
+        <source>Latin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="languages/languages.h" line="52"/>
+        <source>Sanskrit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qt/mainwindow.cpp" line="551"/>
+        <source>Open a DipyDoc directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
