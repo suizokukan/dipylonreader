9f6e2d6cdad9609eb9a27c1b34a7b39b4d600d1e
suizokukan <suizokukan@orange.fr>
2014-09-12 17:42:31 +0200

    modified all header files : Dipylon > DipylonReader

--- cppdipylon/tests/testhash.cpp
+++ cppdipylon/tests/testhash.cpp
@@ -1,25 +1,25 @@
 /*******************************************************************************
 
-    Dipylon Copyright (C) 2008 Xavier Faure
+    DipylonReader Copyright (C) 2008 Xavier Faure
     Contact: faure dot epistulam dot mihi dot scripsisti at orange dot fr
 
-    This file is part of Dipylon.
-    Dipylon is free software: you can redistribute it and/or modify
+    This file is part of DipylonReader.
+    DipylonReader is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Dipylon is distributed in the hope that it will be useful,
+    DipylonReader is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Dipylon.  If not, see <http://www.gnu.org/licenses/>.
+    along with DipylonReader.  If not, see <http://www.gnu.org/licenses/>.
 
     ____________________________________________________________________________
 
-    ❏Dipylon❏ : tests/testhash.cpp
+    ❏DipylonReader❏ : tests/testhash.cpp
 
     ⇨ tests of the hash function
 
@@ -44,7 +44,7 @@
   creating two (hopefully) different hashes from two different pairs of integers.
 */
 void TestHash::test1a()
-{ 
+{
   int v1 = 1;
   int v2 = 2;
   std::size_t hash1 = 0;
@@ -64,7 +64,7 @@ void TestHash::test1a()
   creating two (hopefully) different hashes from two different pairs of integers.
 */
 void TestHash::test1b()
-{ 
+{
   int v1 = 1;
   int v2 = 2;
   std::size_t hash1 = 0;
