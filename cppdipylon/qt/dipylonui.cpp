52f9d639bffda05a2c12cf3fe26f6d9cc62960e4
suizokukan <suizokukan@orange.fr>
2014-09-14 20:22:18 +0200

    updated splashscreen and icons. Fixed some filenames and removed useless .png files in art/ directory

--- cppdipylon/qt/dipylonui.cpp
+++ cppdipylon/qt/dipylonui.cpp
@@ -271,15 +271,12 @@ int DipylonUI::go(int argc, char **argv) {
                              Qt::WindowStaysOnTopHint);
 
   if (this->first_launch == true || this->display_splashscreen == true) {
-    QString msg("<span style=\"color:#223399\">" + \
-                QObject::tr("<b>Δίπυλον project</b> : Dipylon reader<br/>"
-                            "- version %1 -<br/>"
-                            "Dipydoc formats accepted : %2 to %3.<br/>"));
+    QString msg("<span style=\"color:#000000\">" + \
+                QObject::tr("<b>Δίπυλον project</b> : Dipylon Reader<br/>"
+                            "- version %1 -<br/>"));
 
     splashscreen.showMessage(msg.arg(fixedparameters::application_version,
-                                     QString().setNum(DipyDoc::min_dipydocformat_version),
-                                     QString().setNum(DipyDoc::max_dipydocformat_version)),
-                             Qt::AlignLeft);
+                             Qt::AlignLeft));
 
     splashscreen.show();
     QTimer::singleShot(fixedparameters::splashscreen_maximal_duration,
