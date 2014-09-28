87fa1cbb3626cd994324d25e0a6e905314162e9e
suizokukan <suizokukan@orange.fr>
2014-09-28 11:42:47 +0200

    new UI with buttons in text zone : added qt/sourcetoolbar.*, qt/sourcezone.*

---
+++ cppdipylon/qt/sourcetoolbar.cpp
@@ -0,0 +1,49 @@
+/*******************************************************************************
+
+    DipylonReader Copyright (C) 2008 Xavier Faure
+    Contact: faure dot epistulam dot mihi dot scripsisti at orange dot fr
+
+    This file is part of DipylonReader.
+    DipylonReader is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DipylonReader is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DipylonReader.  If not, see <http://www.gnu.org/licenses/>.
+
+    ____________________________________________________________________________
+
+    ❏DipylonReader❏ : qt/sourcetoolbar.cpp
+
+    See sourcetoolbar.h for the documentation.
+
+*******************************************************************************/
+
+#include "qt/sourcetoolbar.h"
+
+/*______________________________________________________________________________
+
+  SourceToolBar::constructor
+______________________________________________________________________________*/
+SourceToolBar::SourceToolBar(UI& _ui) : ui(_ui) {
+  DebugMsg() << "SourceToolBar::SourceToolBar : entry point";
+
+  this->setOrientation(Qt::Vertical);
+
+  this->addAction(this->ui.mainWin->readingmodeAct);
+  this->addAction(this->ui.mainWin->audiocontrols_playAct);
+  this->addAction(this->ui.mainWin->audiocontrols_stopAct);
+  this->addSeparator();
+  this->addAction(this->ui.mainWin->textplusAct);
+  this->addAction(this->ui.mainWin->textminusAct);
+
+  this->setFloatable(true);
+
+  DebugMsg() << "SourceToolBar::SourceToolBar : exit point";
+}
