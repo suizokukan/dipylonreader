965b2183b0b9ea7dc88924a1e68ed370b01b8e5f
suizokukan <suizokukan@orange.fr>
2014-10-01 23:51:48 +0200

    Improved the way the target files are found so that there's no doubloon left.

--- cppdipylon/cpplint.py
+++ cppdipylon/cpplint.py
@@ -2,7 +2,7 @@
 #
 # create_demo_packages.py
 #
-# Python2 script.
+# Python2/3 script.
 # to be used only on a system with a shell.
 #
 ################################################################################
@@ -11,6 +11,8 @@
 # the 'cpplint_py__output' file.
 # 
 ################################################################################
+# version 2 (2014.10.01) : improved the way the target files are found so that
+#                          there's no doubloon left.
 # version 1 (2014.10.01) : first version to be committed.
 ################################################################################
 
@@ -18,12 +20,12 @@ import os
 
 invoking_cpplint = "python2 ~/projets/cpplint.py --linelength=120"
 
-paths_to_be_analyzed = ('./debugmsg', 
-                        './dipydoc', 
-                        './languages', 
-                        './misc', 
-                        './pos', 
-                        './qt')
+paths_to_be_analyzed = ('debugmsg', 
+                        'dipydoc', 
+                        'languages', 
+                        'misc', 
+                        'pos', 
+                        'qt')
 
 #...............................................................................
 # system call
@@ -44,8 +46,7 @@ ossystem("rm -f cpplint_py__output")
 target_directories = []
 for _root, _dirs, _files in os.walk("."):
     for target_dir in _dirs:
-        if _root in paths_to_be_analyzed or \
-           "./"+target_dir in paths_to_be_analyzed:
+        if _root == '.' and target_dir in paths_to_be_analyzed:
             target_directories.append(_root+"/"+target_dir)
 
 target_directories = sorted(target_directories)
@@ -58,7 +59,7 @@ for target_dir in target_directories:
         for _file in files:
             if _file.endswith(".cpp") or _file.endswith(".h"):
                 ossystem("{0} \"{1}\" &>>cpplint_py__tmp".format(invoking_cpplint,
-                                                                 target_dir+"/"+_file))
+                                                                 root+"/"+_file))
 
                 ossystem("cat cpplint_py__tmp >> cpplint_py__output")
                 ossystem("rm cpplint_py__tmp")
