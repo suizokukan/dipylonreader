7d0d533b0acfacfb5431a0bbbb5972132af3942d
suizokukan <suizokukan@orange.fr>
2014-11-11 21:21:59 +0100

    updated build_all.sh : fixed the name of the building scripts

--- cppdipylon/build_all.sh
+++ cppdipylon/build_all.sh
@@ -1,13 +1,13 @@
 rm ../builds/*
 
-rm -rf temp__build* && ./build_dipylonreader_win32static.py --debug=0 --console=no
-rm -rf temp__build* && ./build_dipylonreader_win32static.py --debug=1 --console=yes
+rm -rf temp__build* && ./build_win32static.py --debug=0 --console=no
+rm -rf temp__build* && ./build_win32static.py --debug=1 --console=yes
 
-rm -rf temp__build* && ./build_dipylonreader_win64static.py --debug=0 --console=no
-rm -rf temp__build* && ./build_dipylonreader_win64static.py --debug=1 --console=yes
+rm -rf temp__build* && ./build_win64static.py --debug=0 --console=no
+rm -rf temp__build* && ./build_win64static.py --debug=1 --console=yes
 
-rm -rf temp__build* && ./build_dipylonreader_linux64dynamic.py --debug=0
-rm -rf temp__build* && ./build_dipylonreader_linux64dynamic.py --debug=1
+rm -rf temp__build* && ./build_linux64dynamic.py --debug=0
+rm -rf temp__build* && ./build_linux64dynamic.py --debug=1
 
 rm -rf temp__build*
 
